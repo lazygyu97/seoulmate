2d79e0eca57c414e70ce406277444e8ca40f1c24
Park Hyeongyu <park970406@me.com>
2023-08-22 16:47:06 +0900

    [init]엔티티 기본 설계 및 의존성 추가

---
+++ src/main/java/com/sparta/seoulmate/entity/Follow.java
@@ -0,0 +1,43 @@
+package com.sparta.seoulmate.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Table(name = "follows")
+public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    // 외래키로 user_id 받아오기
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; // 나
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_user_id", nullable = false)
+    private User followingUser; // 내가 팔로우 하는 사용자 나 -> 사용자.
+
+    //팔로잉 한 일시
+    @Column
+    private LocalDateTime followingDateTime;
+
+}

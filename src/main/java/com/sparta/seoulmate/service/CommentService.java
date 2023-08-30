a8b6ef49d3ec97075a47aa76d60dea51b0575c7d
Park Hyeongyu <park970406@me.com>
2023-08-30 09:58:23 +0900

    [refactor] 머지후 수정 -댓글 좋아요 관련 기능들이었던 CommentLikeController,Service에 있던 내용을 CommentController,Service로 옮기고 삭제했습니다. -좋아요 관련 기능들의 메서드 명을 통일 시켰습니다.

--- src/main/java/com/sparta/seoulmate/service/CommentService.java
+++ src/main/java/com/sparta/seoulmate/service/CommentService.java
@@ -3,12 +3,11 @@ package com.sparta.seoulmate.service;
 import com.sparta.seoulmate.dto.comment.CommentListResponseDto;
 import com.sparta.seoulmate.dto.comment.CommentRequestDto;
 import com.sparta.seoulmate.dto.comment.CommentResponseDto;
-import com.sparta.seoulmate.entity.Comment;
-import com.sparta.seoulmate.entity.Post;
-import com.sparta.seoulmate.entity.User;
-import com.sparta.seoulmate.entity.UserRoleEnum;
+import com.sparta.seoulmate.entity.*;
+import com.sparta.seoulmate.repository.CommentLikeRepository;
 import com.sparta.seoulmate.repository.CommentRepository;
 import com.sparta.seoulmate.repository.PostRepository;
+import com.sparta.seoulmate.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +20,7 @@ public class CommentService {
 
     private final PostRepository postRepository;
     private final CommentRepository commentRepository;
+    private final CommentLikeRepository commentLikeRepository;
 
     // Comment 생성
     public CommentResponseDto createComment (Long postId, User user, CommentRequestDto commentRequestDto) {
@@ -74,6 +74,64 @@ public class CommentService {
             commentRepository.delete(comment);
         } else {
             throw new IllegalStateException("댓글 삭제 권한이 없습니다.");
+        }
+    }
+
+    @Transactional
+    public CommentResponseDto likeComment(UserDetailsImpl userDetails, Long commentId) throws Exception {
+
+        // 토큰 체크
+        User user = userDetails.getUser();
+        if (user == null) {
+            throw new IllegalStateException("사용자 정보를 찾을 수 없습니다.");
+        }
+
+        // 좋아요 누른 댓글 조회
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글을 찾을 수 없습니다."));
+
+        // 좋아요 누른 댓글이 본인 댓글이면 좋아요 불가능
+        if (user.getId().equals(comment.getAuthor().getId())) {
+            throw new IllegalArgumentException("자신의 댓글에는 좋아요 할 수 없습니다.");
+        }
+
+        // 중복 좋아요 방지
+        CommentLike commentLike = commentLikeRepository.findByCommentIdAndUserId(comment.getId(), user.getId());
+        if (commentLike != null){
+            throw new IllegalArgumentException("이미 좋아요를 하고 있습니다.");
+        }
+
+        // commentLikeRepository DB저장
+        CommentLike sevedCommentLike = CommentLike.builder()
+                .comment(comment)
+                .user(user)
+                .build();
+        commentLikeRepository.save(sevedCommentLike);
+
+        // CommentResponseDto 생성
+        CommentResponseDto commentResponseDto = CommentResponseDto.of(comment);
+
+        return commentResponseDto;
+    }
+
+    public void deleteLikeComment(UserDetailsImpl userDetails, Long commentId) {
+        // 토큰 체크
+        User user = userDetails.getUser();
+        if (user == null) {
+            throw new IllegalStateException("사용자 정보를 찾을 수 없습니다.");
+        }
+
+        // 좋아요 누른 댓글 조회 (좋아요가 눌려 있지 않으면 삭제 대상이 아님)
+        CommentLike commentLike = commentLikeRepository.findByCommentIdAndUserId(commentId, user.getId());
+        if (commentLike == null){
+            throw new IllegalStateException("이미 좋아요를 하고 있지 않습니다.");
+        }
+
+        // 좋아요 누른 본인이거나 admin 경우만 삭제 가능
+        if (user.getId().equals(commentLike.getUser().getId())) {
+            commentLikeRepository.delete(commentLike);
+        } else {
+            throw new IllegalStateException("좋아요 취소 권한이 없습니다.");
         }
     }
 
